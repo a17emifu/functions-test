b43b2257f22842892c81d8732872a0f645f8a7b8
a17emifu <kamonabe0@gmail.com>
2021-09-28 12:59:37 +0200

    DB接続情報をGithub上から削除

--- src/db/config.ts
+++
@@ -1,7 +0,0 @@
-const config = {
-  endpoint: "https://fwywd-test.documents.azure.com:443/",
-  key: "MoQ39LJgdNkrSOQOg3c8bcv6qrXSWnJm9a3MdaDxKo5kyvIOt48lkVxG5xIDZMAbBk6NGSCwqHVpX63si3KAuQ==",
-  databaseId: "TestDB",
-};
-
-export default config;
